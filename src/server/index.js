4ba499ef019756d1ed7f748404eb121886455247
Jef De Smedt <Jef.DeSmedt@betavzw.org>
2019-03-01 06:36:16 +0100

    api cursisten toegevoegd

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,7 @@ const app = express();
 const root = path.join(__dirname, '../../');
 const router = express.Router();
 const cursussen = [{id:1, naam:'React'}, {id: 2, naam:'Angular'}]
-const deelnemers = [];
+const cursisten = [{id:1, naam:'Karen Damen'}, {id:2, naam:'Kristel Verbeke'}, {id:3, naam:'Josje Huisman'}];
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/', express.static(path.join(root, 'src/client/opleiding/build')));
@@ -17,14 +17,26 @@ router.use(function(req, res, next) {
 router.route('/cursussen').get((req, resp) =>{
     resp.json(cursussen);
 });
+router.route('/cursisten').get((req, resp) =>{
+    resp.json(cursisten);
+});
 router.route('/cursussen').post((req, resp) =>{
     let cursus = {id: cursussen.length, naam: req.body.naam};
     cursussen.push(cursus);
     resp.json(cursus);
 });
+router.route('/cursisten').post((req, resp) =>{
+    let cursist = {id: cursisten.length, naam: req.body.naam};
+    cursussen.push(cursist);
+    resp.json(cursist);
+});
 router.route('/cursussen/:cursusid').get((req, resp) => {
     const cursus = cursussen.find( c => c.id == req.params.cursusid);
     resp.json(cursus);
+})
+router.route('/cursisten/:cursistid').get((req, resp) => {
+    const cursist = cursisten.find( c => c.id == req.params.cursistid);
+    resp.json(cursist);
 })
 app.use('/api', router);
 
